26216472df43f9ffa1d1f1935ec4c7d2988dbba6
David Liu <david.s.liu@gmail.com>
2023-06-25 16:39:46 +0000

    React Select with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Select, { SingleValue } from "react-select";
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+interface IOptions {
+  value: string;
+  label: string;
+}
+
+const App = () => {
+  const options: IOptions[] = [
+    { value: "blues", label: "Blues" },
+    { value: "rock", label: "Rock" },
+    { value: "jazz", label: "Jazz" },
+    { value: "orchestra", label: "Orchestra" },
+  ];
+
+  const [selected, setSelected] = useState<IOptions | null>(null);
+  const [rating, setRating] = useState<number>(0);
+
+  const handleSelected = (selectedOption: SingleValue<IOptions>) => {
+    setSelected(selectedOption);
+    console.log(`Option selected:`, selected);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="card">What category of music do you like?</h2>
+      <div className="mt-5 m-auto w-50">
+        <Select options={options} onChange={handleSelected} autoFocus={true} />
+
+        <div className="mt-4">
+          {selected && <>You've selected {selected.value}</>}
+        </div>
+      </div>
+      {/* select={setRating} selected={rating} */}
+      <h2 className="card">How would you rate your service with us?</h2>
+      <ul className="rating">
+        {Array.from({ length: 10 }, (_, i) => (
+          <li key={`rating-${i + 1}`}>
+            <input
+              type="radio"
+              id={`num${i + 1}`}
+              name="rating"
+              value={i + 1}
+              onChange={(e) => setRating(Number(e.target.value))}
+              checked={rating === i + 1}
+            />
+            <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
